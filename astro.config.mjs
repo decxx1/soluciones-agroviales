4a2a893e2bc290f2add1f4687f0d652a77cd778a
Damian Cisternas <decxx1@gmail.com>
2024-05-21 15:45:15 -0300

    init project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'static',
+  site: 'https://solucionesagrovialeshm.com.ar/',
+  build: {
+    // Ejemplo: Genera `page.html` en lugar de `page/index.html` durante la compilación.
+    format: 'file'
+  },
+  integrations: [tailwind(), react()]
+});
