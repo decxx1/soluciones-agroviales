0eed4999952f3aa6908a813955bec10e008178be
Damian Cisternas <decxx1@gmail.com>
2024-05-27 15:18:04 -0300

    Contact form and About page

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -22,6 +22,16 @@ export default function Menu () {
                         className={ classMenu('/') + ' block py-2 pr-4 pl-3  border-b border-gray-100  lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700'}
                         aria-current="page">Inicio</a>
                 </li>
+                <li>
+                    <a href="/about"
+                        className={ classMenu('/about') + ' block py-2 pr-4 pl-3  border-b border-gray-100  lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700'}
+                        aria-current="page">Sobre nosotros</a>
+                </li>
+                <li>
+                    <a href="/contact"
+                        className={ classMenu('/contact') + ' block py-2 pr-4 pl-3  border-b border-gray-100  lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700'}
+                        aria-current="page">Contacto</a>
+                </li>
             </ul>
         </>
     )
