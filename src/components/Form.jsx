791c127e2f49ed3a2f0c0c745c161493386c3c37
Damian Cisternas <decxx1@gmail.com>
2024-08-12 14:18:48 -0300

    Update astro, new endpoint form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { useState } from 'react';
 import { 
     secretKey,
+    siteKey,
     endPoint,
 } from '@/hooks/env.js';
 
@@ -15,12 +16,14 @@ export default function Form () {
     const handleSubmit = (event) => {
         event.preventDefault();
         const fields = Object.fromEntries(new FormData(event.target))
+        fields.secret_key = secretKey;
+        fields.addressee = 'decxx1@gmail.com';
+        fields.asunto = "Contacto desde la web - de: " + fields.name;
         if(!isSending){
             setIsSending(true);
             grecaptcha.ready(function() {
-                grecaptcha.execute(secretKey, { action: 'contacto' }).then(function(getToken) {
+                grecaptcha.execute(siteKey, { action: 'contacto' }).then(function(getToken) {
                     fields.token = getToken;
-                    fields.action = 'contacto';
                     
                     sendForm(fields);
                 });
@@ -38,7 +41,7 @@ export default function Form () {
             resetForm()
         })
         .catch(error => {
-            console.error(error)
+            //console.error(error)
             toast.error('No se pudo enviar el mensaje vuelva a intentarlo más tarde.')
         })
         .finally(() => {
@@ -50,11 +53,11 @@ export default function Form () {
             <Toaster richColors position="top-right" />
             <div className="sm:col-span-2">
                 <label forhtml="first-name" className="block mb-2 text-sm font-medium text-gray-200 dark:text-gray-300">Nombre</label>
-                <input type="text" id="first-name" name="nombre" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Nombre" required/>
+                <input type="text" id="first-name" name="name" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Nombre" required/>
             </div>
             <div>
                 <label forhtml="telphone" className="block mb-2 text-sm font-medium text-gray-200 dark:text-gray-300">Teléfono</label>
-                <input type="number" id="telphone" name="telefono" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Teléfono" required/>
+                <input type="number" id="telphone" name="phone" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Teléfono" required/>
             </div>
             <div>
                 <label forhtml="email" className="block mb-2 text-sm font-medium text-gray-200 dark:text-gray-300">E-mail</label>
@@ -63,7 +66,7 @@ export default function Form () {
             
             <div className="sm:col-span-2">
                 <label forhtml="message" className="block mb-2 text-sm font-medium text-gray-200 dark:text-gray-400">Mensaje</label>
-                <textarea id="message" name="mensaje" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Deje su consulta..." required></textarea>
+                <textarea id="message" name="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Deje su consulta..." required></textarea>
             </div>
             <button disabled={isSending} type="submit" className="py-3 px-5 text-sm font-medium text-center text-gray-700 rounded bg-primary-300 sm:w-fit hover:bg-primary-400 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-300 dark:hover:bg-primary-400 dark:focus:ring-primary-400 dark:text-gray-800">Enviar mensaje</button>
         </form>
